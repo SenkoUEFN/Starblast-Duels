26e94d11cc363b92b0e4f66b77fb959b15586b24
SenkoUEFN <Senko.uefn@gmail.com>
2026-07-26 16:35:01 +0200

    first commit

---
+++ public/main.js
@@ -0,0 +1,68 @@
+const playButton = document.querySelector("#playButton")
+const nameInput = document.querySelector("#nameInput")
+const title = document.querySelector("#title")
+const waitForPlayersText = document.querySelector("#waitForPlayers")
+let waitForPlayers = false
+let noEcpKey = false
+
+playButton.addEventListener("click", joinGame)
+
+const socket = new WebSocket("ws://localhost:9500")
+const id = crypto.randomUUID()
+let gameId
+
+socket.addEventListener("message", (message) =>
+{
+    let msg = JSON.parse(message.data)
+    console.log(msg)
+    if(msg.name === "room_created")
+    {
+        gameId = msg.data.id
+        waitForPlayers = false
+        noEcpKey = false
+        joinRoom()
+        
+    }
+    else if(msg.name === "wait for players")
+    {
+        waitForPlayers = true
+        showWaitForPlayers()
+    }
+})
+
+function joinGame()
+{
+    const name = nameInput.value
+
+    socket.send(JSON.stringify(
+        {
+            name : "join_game",
+            data : 
+            {
+                id : id,
+                name : name,
+                ecpKey : localStorage.getItem("ECPKey")
+            }
+        }
+    ))
+}
+
+function joinRoom()
+{
+    window.open(`https://starblast.io/#${gameId}@195.201.89.106:3009`,"_blank")
+    console.log(`https://starblast.io/#${gameId}@195.201.89.106:3009`)
+}
+
+function showWaitForPlayers()
+{
+    console.log("wait players")
+    playButton.style.display = "none"
+    nameInput.style.display = "none"
+    title.style.display = "none"
+    waitForPlayersText.style.display = "block"
+}
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,230 @@
+const express = require("express")
+const path = require("path")
+const WebSockets = require("ws")
+const app = express()
+const port = 9000
+const wsPort = 9500
+let rooms = []
+let waitingList = []
+let waitingListServer = []
+
+app.listen(port, () =>
+{
+    console.log("server running on :", `http://localhost:${port}`)
+})
+
+app.use(express.static(
+    path.join(__dirname, "public")
+))
+
+const wss = new WebSockets.WebSocketServer(
+    {
+        port : wsPort
+    }
+)
+
+wss.on("connection", (socket) =>
+{
+    socket.send(JSON.stringify(
+        {
+            name : "connected"
+        }
+    ))
+
+    socket.on("message", (message) =>
+    {
+        let msg = JSON.parse(message)
+        if(msg.name === "join_game")
+        {
+            newPlayer(msg.data, socket, msg.data.ecpKey)
+        }  
+    })
+})
+
+function newPlayer(playerInfo, socket, ecpKey)
+{   
+    for(player of waitingList)
+    {
+        if(player.playerInfo.id === playerInfo.id)
+        {
+            return
+        }
+    }
+    const dataServer = 
+    {
+        playerInfo : playerInfo,
+        socket : socket,
+        ecpKey : ecpKey
+    }
+    const dataClient =
+    {
+        playerInfo : playerInfo,
+        socket : socket
+    }
+    waitingListServer.push(dataServer)
+    waitingList.push(dataClient)
+    for(player of waitingList)
+    {
+        console.log(player.playerInfo)
+    }
+    if(waitingList.length >= 2)
+    {
+        createRoom()
+    }
+    else
+    {
+        waitForPlayers(waitingList)
+    }
+}
+
+async function createModdingGame(players)
+{
+    let ecpKey
+    for(player of players)
+    {
+        if(player.ecpKey !== undefined)
+        {
+            ecpKey = player.ecpKey
+            break
+        }
+    }
+    if(ecpKey === undefined) { return "no ecp key" }
+    return new Promise((resolve, reject) =>
+    {
+        
+        const serverWsAdress = "wss://195-201-89-106.starblast.io:3009/"
+        let token
+        let roomData = {}
+        const tokenSocket = new WebSockets(
+            serverWsAdress,
+            {
+                headers :
+                {
+                    Origin : "https://starblast.io"
+                }
+            }
+        )
+        tokenSocket.on("open", () =>
+        {
+            tokenSocket.send(JSON.stringify(
+                {
+                    name : "modding_token",
+                    data :
+                    {
+                        ecp_key : ecpKey
+                    }
+                }
+            ))
+        })
+        tokenSocket.on("message", (message) =>
+        {
+            let msg = JSON.parse(message)
+            console.log(msg)
+            if(msg.name === "token")
+            {
+                token = msg.data.token
+                createMod()
+            }
+            
+        })
+
+        function createMod(){    
+            const modSocket = new WebSockets(
+                serverWsAdress,
+                {
+                    headers : 
+                    {
+                        Origin : "https://starblast.data.neuronality.com"
+                    }
+                }
+            )
+
+            modSocket.on("open", () =>
+            {
+                console.log(token)
+                modSocket.send(JSON.stringify(
+                    {
+                        name : "run_mod",
+                        data : 
+                        {
+                            token : token,
+                            options :
+                            {
+                                root_mode: "survival",
+                                map_size : 30,
+                                starting_ship : 605,
+                                starting_ship_maxed : true,
+                                max_level : 6,
+                                max_players : 2,
+                                custom_map : ""
+                            }
+                        }
+                    }
+                ))
+            })
+
+            modSocket.on("message", (message) =>
+            {
+                let msg = JSON.parse(message)
+                if(msg.name === "tick" || msg.name === "ship_update") { return }
+                console.log(msg)
+                if(msg.name === "mod_started")
+                {
+                    roomData = 
+                    {
+                        id : msg.data.id,
+                        serverWsAdress : serverWsAdress,
+                        players : players
+                    }
+                    resolve(roomData)
+                }
+            })
+        }
+    })
+    
+}
+
+function waitForPlayers(players)
+{
+    for(player of players)
+    {
+        player.socket.send(JSON.stringify(
+            {
+                name : "wait for players"
+            }
+        ))
+    }
+}
+
+async function createRoom()
+{
+    console.log("create Room")
+    const roomPlayers = [waitingList[0],waitingList[1]]
+    const roomPlayers2 = [waitingListServer[0],waitingListServer[1]]
+    waitingList.splice(0, 2)
+    const room = await createModdingGame(roomPlayers2)
+    if(room === "no ecp key")
+    {
+        for(player of roomPlayers)
+        {
+            player.socket.send(JSON.stringify(
+                {
+                    name : "no ecp key"
+                }
+            ))
+        }
+        return
+    }
+    for(player of roomPlayers)
+    {
+        player.socket.send(JSON.stringify(
+            {
+                name : "room_created",
+                data : room
+            }
+        ))
+    }
+}
+
+
+
